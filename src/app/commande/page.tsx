4824010edd67fdce53037c3c72ef5d73caebe02c
Achouak Cherif <65472852+Achwak28@users.noreply.github.com>
2024-04-18 05:50:25 +0200

    Update page.tsx

--- src/app/commande/page.tsx
+++ src/app/commande/page.tsx
@@ -10,34 +10,53 @@ const page = () => {
     const [selectedItemIndex2, setSelectedItemIndex2] = useState(0);
     const [selectedItemIndex3, setSelectedItemIndex3] = useState(0);
 
-    const handleItemClick = (index : number) => {
+    const handleItemClick = (index: number) => {
         setSelectedItemIndex(index);
     };
 
 
 
-    const handleItemClick2 = (index : number) => {
+    const handleItemClick2 = (index: number) => {
         setSelectedItemIndex2(index);
     };
-    const handleItemClick3 = (index : number) => {
+    const handleItemClick3 = (index: number) => {
         setSelectedItemIndex3(index);
     };
     return <div className="commande__container">
         <div className="commande__hero">
-        <img src="./assets/bg-commande.png" alt="commande background" className="commande__background" />
-        <div className="hero__text">
-            <p>Commande</p>
-        </div>
+            <img src="./assets/bg-commande.png" alt="commande background" className="commande__background" />
+            <div className="hero__text">
+                <p>Commande</p>
+            </div>
         </div>
         <div className="red__p">
             <p>Pour les demandes passées moins de 2 heures ouvrée avant l'heure prévue de chargement, l'annulation de la course est facturée à 50% du prix de la course, soit 715.50€ . L'annulation est facturée à 100% du prix de la commande si le chauffeur a démarré son approche vers le point de chargement.</p>
         </div>
         <div className="choose__van">
             <div className="choose__van-left">
-                <MultiItems title="Choisissez le Supervan qui vous convient" items={supervans} selectedItemIndex={selectedItemIndex} onItemClick={handleItemClick} textPosition="bottom-right" />
-                <MultiItems title="Prestation de manutention" items={trolley} selectedItemIndex={selectedItemIndex2} onItemClick={handleItemClick2} textPosition="top-left" />
-                <MultiItems title="Choisissez le Supervan qui vous convient" items={supervans2} selectedItemIndex={selectedItemIndex3} onItemClick={handleItemClick3} textPosition="bottom-right" />
+                <MultiItems
+                    title="Choisissez le Supervan qui vous convient"
+                    items={supervans}
+                    selectedItemIndex={selectedItemIndex}
+                    onItemClick={handleItemClick}
+                    textPosition="bottom-right"
+                />
+                <MultiItems
+                    title="Prestation de manutention"
+                    items={trolley}
+                    selectedItemIndex={selectedItemIndex2}
+                    onItemClick={handleItemClick2}
+                    textPosition="top-left"
+                />
+                <MultiItems
+                    title="Choisissez le Supervan qui vous convient"
+                    items={supervans2}
+                    selectedItemIndex={selectedItemIndex3}
+                    onItemClick={handleItemClick3}
+                    textPosition="bottom-right"
+                />
             </div>
+
             <div className="choose__van-right">
                 <div className="price-box">
                     <p>
